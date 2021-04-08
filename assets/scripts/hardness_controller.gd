9732581bf6018ae837e0a7d3de990bf87b6e5dbe
daniilbelov <tseflutty@yandex.ru>
2021-04-08 23:53:21 +0300

    Add hardness controller

---
+++ assets/scripts/hardness_controller.gd
@@ -0,0 +1,32 @@
+class_name HardnessController
+extends Node
+
+
+var asteroid_generator : PAsteroidGenerator
+
+
+export var __asteroid_generator : NodePath setget _set__asteroid_generator
+func _set__asteroid_generator(value : NodePath):
+	__asteroid_generator = value
+	var node : Node = get_node_or_null(value)
+	if node is PAsteroidGenerator:
+		asteroid_generator = node as PAsteroidGenerator
+
+
+export var score_to_hardness_up : int = 40
+
+
+export var hardness_up_asteroid_speed : float = 10
+
+export var hardness_up_asteroid_spawn_delay : float = -0.5
+
+
+func _ready():
+	call_deferred("_set__asteroid_generator", __asteroid_generator)
+	add_to_group("PlayerScoreListener")
+
+
+func on_PlayerScore_updated(score_node : PlayerScore):
+	if score_node.score % self.score_to_hardness_up == 0:
+		self.asteroid_generator.spawn_delay += self.hardness_up_asteroid_spawn_delay
+		self.asteroid_generator.asteroids_speed += self.hardness_up_asteroid_speed
