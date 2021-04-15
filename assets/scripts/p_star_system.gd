9a76aa6dfd200cd5d4d7c0085886a07538872770
daniilbelov <tseflutty@yandex.ru>
2021-04-15 18:27:32 +0300

    Clean star system

--- assets/scripts/p_star_system.gd
+++ assets/scripts/p_star_system.gd
@@ -8,10 +8,6 @@ extends Spatial
 var PPlanetOrbit_orbits : Array
 
 
-func get_selected_orbit() -> PPlanetOrbit:
-	return self.PPlanetOrbit_orbits[self.selected_orbit_index]
-
-
 export(Array, NodePath) var __PPlanetOrbit_orbits : Array setget _set__PPlanetOrbit_orbits
 func _set__PPlanetOrbit_orbits(value : Array) -> void:
 	__PPlanetOrbit_orbits = value
